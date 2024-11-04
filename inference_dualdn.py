2387ca3aa0db23eb564ecb552c1948be356855b3
Ruikang Li <54311152+Lyricccco@users.noreply.github.com>
2024-11-04 22:55:46 +0800

    Add files via upload

---
+++ inference_dualdn.py
@@ -0,0 +1,261 @@
+# -----------------------------------------------------------------------------------
+# [ECCV2024] DualDn: Dual-domain Denoising via Differentiable ISP 
+# [Homepage] https://openimaginglab.github.io/DualDn/
+# [Author] Originally Written by Ruikang Li, from MMLab, CUHK.
+# [License] Absolutely open-source and free to use, please cite our paper if possible. :)
+# -----------------------------------------------------------------------------------
+
+import os
+import yaml
+import torch
+import random
+import logging
+import argparse
+import numpy as np
+from copy import deepcopy
+from os import path as osp
+from collections import OrderedDict
+
+os.environ['CUDA_VISIBLE_DEVICES'] = "0"
+
+from models import build_model
+from data import build_dataset, build_dataloader
+from utils import get_root_logger, get_time_str, make_exp_dirs, dict2str, get_dist_info, init_dist
+
+def set_random_seed(seed):
+    """Set random seeds."""
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+
+def _postprocess_yml_value(value):
+    # None
+    if value == '~' or value.lower() == 'none':
+        return None
+    # bool
+    if value.lower() == 'true':
+        return True
+    elif value.lower() == 'false':
+        return False
+    # !!float number
+    if value.startswith('!!float'):
+        return float(value.replace('!!float', ''))
+    # number
+    if value.isdigit():
+        return int(value)
+    elif value.replace('.', '', 1).isdigit() and value.count('.') < 2:
+        return float(value)
+    # list
+    if value.startswith('['):
+        return eval(value)
+    # str
+    return value
+
+def ordered_yaml():
+    """Support OrderedDict for yaml.
+
+    Returns:
+        tuple: yaml Loader and Dumper.
+    """
+    try:
+        from yaml import CDumper as Dumper
+        from yaml import CLoader as Loader
+    except ImportError:
+        from yaml import Dumper, Loader
+
+    _mapping_tag = yaml.resolver.BaseResolver.DEFAULT_MAPPING_TAG
+
+    def dict_representer(dumper, data):
+        return dumper.represent_dict(data.items())
+
+    def dict_constructor(loader, node):
+        return OrderedDict(loader.construct_pairs(node))
+
+    Dumper.add_representer(OrderedDict, dict_representer)
+    Loader.add_constructor(_mapping_tag, dict_constructor)
+    return Loader, Dumper
+
+def yaml_load(opt_file, test_mode, iters):
+    """Load yaml file or string.
+
+    Args:
+        f (str): Option file path or a python string.
+
+    Returns:
+        (dict): Options.
+    """
+    if osp.isfile(opt_file):
+        with open(opt_file, mode='r') as f:
+            loaded_opt = yaml.load(f, Loader=ordered_yaml()[0])
+    else:
+        loaded_opt = yaml.load(opt_file, Loader=ordered_yaml()[0])
+    loaded_opt['is_train'] = not test_mode
+    loaded_opt['opt_path'] = opt_file
+    loaded_opt['iters'] = iters
+
+    return loaded_opt
+
+def opt_update(opt):
+    """Update option file or string for individent user.
+    Args:
+        opt(dict): Loaded Options dict.
+
+    Returns:
+        (dict): Updated Options dict.
+    """
+
+    if opt['num_gpu'] == 'auto':
+        opt['num_gpu'] = torch.cuda.device_count()
+
+    # datasets
+    for phase, dataset in opt['datasets'].items():
+        dataset['phase'] = phase
+        if dataset.get('data_path') is not None:
+            dataset['data_path'] = osp.expanduser(dataset['data_path'])
+
+    # paths
+    for key, val in opt['path'].items():
+        if (val is not None) and ('resume_state' in key or 'pretrain_network' in key):
+            opt['path'][key] = osp.expanduser(val)
+    # add root path
+    opt['root_path'] = osp.abspath(osp.join(__file__, osp.pardir))
+    opt['experiment_path'] = osp.abspath(osp.join(opt['opt_path'], osp.pardir))
+
+    results_root = osp.join(opt['root_path'], 'results', opt['name'])
+    opt['path']['log'] = results_root
+    opt['path']['results_root'] = results_root
+    opt['path']['visualization'] = results_root
+    opt['path']['pretrain_network'] = osp.join(opt['experiment_path'], 'models', 'net_g_{}.pth'.format(opt['iters']))
+    opt['path']['param_key'] = 'param_key'
+
+    return opt
+
+def parse_options():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('-opt', type=str, default=r'./experiments/5000_0.0002-0.02_DualDn_Restormer/denoising.yml', help='Path to option YAML file.')
+    parser.add_argument('--num_iters', type=int, default=300000, help='num iterations')
+    parser.add_argument('--launcher', choices=['none', 'pytorch', 'slurm'], default='none', help='job launcher')
+    parser.add_argument('--test', default=True, help='Test/Train mode')
+    parser.add_argument('--local_rank', type=int, default=0) 
+    parser.add_argument('--force_yml', nargs='+', default=None, help='Force to update yml files. Examples: train:ema_decay=0.999')
+    args = parser.parse_args()
+
+    ##* load and update yaml option file
+    opt = yaml_load(args.opt, args.test, args.num_iters)
+    opt = opt_update(opt)
+
+    ##* distributed-training settings
+    if args.launcher == 'none':
+        opt['dist'] = False
+        print('Disable distributed.', flush=True)
+    else:
+        opt['dist'] = True
+        if args.launcher == 'slurm' and 'dist_params' in opt:
+            init_dist(args.launcher, **opt['dist_params'])
+        else:
+            init_dist(args.launcher)
+        print('init dist .. ', args.launcher)
+    opt['rank'], opt['world_size'] = get_dist_info()
+
+    ##* random seed
+    if opt['manual_seed'] is None:
+        seed = random.randint(1, 10000)
+        opt['manual_seed'] = seed
+    seed = opt.get('manual_seed')
+    for _, dataset in opt['datasets'].items():
+        dataset['seed'] = seed
+    set_random_seed(seed + opt['rank'])
+
+    ##* force to update yml options
+    if args.force_yml is not None:
+        for entry in args.force_yml:
+            # now do not support creating new keys
+            keys, value = entry.split('=')
+            keys, value = keys.strip(), value.strip()
+            value = _postprocess_yml_value(value)
+            eval_str = 'opt'
+            for key in keys.split(':'):
+                eval_str += f'["{key}"]'
+            eval_str += '=value'
+            # using exec function
+            exec(eval_str)
+
+    return opt
+
+def main():
+    # parse options, set distributed setting, set ramdom seed
+    opt = parse_options()
+
+    torch.backends.cudnn.benchmark = True
+
+    # mkdir and initialize loggers
+    make_exp_dirs(opt)
+    log_file = osp.join(opt['path']['log'],
+                        f"test_{opt['name']}_{get_time_str()}.log")
+    logger = get_root_logger(
+        logger_name='basicsr', log_level=logging.INFO, log_file=log_file)
+    logger.info(dict2str(opt))
+
+    # create test dataset and dataloader
+    test_loaders = []
+    for phase, dataset_opt in sorted(opt['datasets'].items()):
+        if phase == 'val':
+            for dataset_type, dataset in dataset_opt['val_datasets'].items():
+                if dataset['mode']:
+                    dataset_opt['dataset_type'] = dataset_type
+                    dataset_opt['data_path'] = dataset['data_path']
+
+                    if dataset_type == 'Synthetic':
+                        noise_levels, alphas = [], []
+                        if type(dataset_opt['syn_noise']['noise_level']) == list:
+                            noise_levels = deepcopy(dataset_opt['syn_noise']['noise_level'])
+                        else:
+                            noise_levels[0] = deepcopy(dataset_opt['syn_noise']['noise_level'])
+                        if type(dataset_opt['syn_isp']['alpha']) == list:
+                            alphas = deepcopy(dataset_opt['syn_isp']['alpha'])
+                        else:
+                            alphas[0] = deepcopy(dataset_opt['syn_isp']['alpha'])
+                        for _, noise_level in enumerate(noise_levels):
+                            for _, alpha in enumerate(alphas):
+                                dataset_opt['syn_noise']['noise_level'] = deepcopy(noise_level)
+                                dataset_opt['syn_isp']['alpha'] = deepcopy(alpha)
+                                val_set = build_dataset(dataset_opt)
+                                test_loader = build_dataloader(
+                                    val_set,
+                                    dataset_opt,
+                                    num_gpu=opt['num_gpu'],
+                                    dist=opt['dist'],
+                                    sampler=None,
+                                    seed=opt['manual_seed'])
+                                logger.info(
+                                    f'Number of val images in {dataset_type} {dataset_opt["name"]}: '
+                                    f'{len(val_set)}')
+                                test_loaders.append(test_loader)
+                    else:
+                        val_set = build_dataset(dataset_opt)
+                        test_loader = build_dataloader(
+                            val_set,
+                            dataset_opt,
+                            num_gpu=opt['num_gpu'],
+                            dist=opt['dist'],
+                            sampler=None,
+                            seed=opt['manual_seed'])
+                        logger.info(
+                                    f'Number of val images in {dataset_type} {dataset_opt["name"]}: '
+                                    f'{len(val_set)}')
+                        test_loaders.append(test_loader)
+
+    # create model
+    model = build_model(opt)
+
+    for test_loader in test_loaders:
+        test_set_name = test_loader.dataset.dataset_type
+        logger.info(f'Testing {test_set_name}...')
+        model.validation(test_loader, opt['iters'], None)
+
+
+if __name__ == '__main__':
+    main()
---
+++ train_dualdn.py
@@ -0,0 +1,124 @@
+import os
+import time
+import torch
+import datetime
+from os import path as osp
+
+os.environ['CUDA_VISIBLE_DEVICES'] = "0"
+
+from utils import (parse_options, load_resume_state, mkdir_and_rename, make_exp_dirs, \
+                    copy_opt_file, init_loggers, check_resume, MessageLogger, AvgTimer)
+from models import build_model
+from data import create_train_val_dataloader
+from data.prefetch_dataloader import CPUPrefetcher, CUDAPrefetcher
+
+
+def main():
+    opt = parse_options()
+
+    torch.backends.cudnn.benchmark = True
+    torch.backends.cudnn.deterministic = False
+
+    ##* load resume states if necessary
+    resume_state = load_resume_state(opt)
+
+    ##* mkdir for experiments and logger
+    if resume_state is None:
+        make_exp_dirs(opt)
+        if opt['logger'].get('use_tb_logger') and 'debug' not in opt['name'] and opt['rank'] == 0:
+            mkdir_and_rename(osp.join(opt['root_path'], 'tb_logger', opt['name']))
+
+    ##* copy the yml file to the experiment root
+    copy_opt_file(opt['opt_path'], opt['path']['experiments_root'])
+
+    ##* initialize loggers
+    logger, tb_logger = init_loggers(opt)
+
+    ##* create train and validation dataloaders
+    train_loader, train_sampler, val_loaders, total_epochs, total_iters = create_train_val_dataloader(opt, logger)
+
+    ##* create model
+    model = build_model(opt)
+
+    ##* resume training
+    if resume_state:  
+        check_resume(opt, resume_state['iter'])
+        model.resume_training(resume_state)
+        logger.info(f"Resuming training from epoch: {resume_state['epoch']}, iter: {resume_state['iter']}.")
+        start_epoch = resume_state['epoch']
+        current_iter = resume_state['iter']
+    else:
+        start_epoch = 0
+        current_iter = 0
+
+    ##* create message logger (formatted outputs)
+    msg_logger = MessageLogger(opt, current_iter, tb_logger)
+
+    ##* dataloader prefetcher
+    prefetch_mode = opt['datasets']['train'].get('prefetch_mode')
+    if prefetch_mode is None or prefetch_mode == 'cpu':
+        prefetcher = CPUPrefetcher(train_loader)
+    elif prefetch_mode == 'cuda':
+        prefetcher = CUDAPrefetcher(train_loader, opt)
+        logger.info(f'Use {prefetch_mode} prefetch dataloader')
+        if opt['datasets']['train'].get('pin_memory') is not True:
+            raise ValueError('Please set pin_memory=True for CUDAPrefetcher.')
+    else:
+        raise ValueError(f"Wrong prefetch_mode {prefetch_mode}. Supported ones are: None, 'cuda', 'cpu'.")
+
+    ##* start training
+    logger.info(f'Start training from epoch: {start_epoch}, iter: {current_iter}')
+    data_timer, iter_timer = AvgTimer(), AvgTimer()
+    start_time = time.time()
+
+    for epoch in range(start_epoch, total_epochs + 1):
+        train_sampler.set_epoch(epoch)
+        prefetcher.reset()
+        train_data = prefetcher.next()
+
+        while train_data is not None:
+            data_timer.record()
+            current_iter += 1
+            if current_iter > total_iters:
+                break
+
+            model.update_learning_rate(current_iter, warmup_iter=opt['train'].get('warmup_iter', -1))
+            model.feed_train_data(train_data)
+            model.optimize_parameters()
+            iter_timer.record()
+            if current_iter == 1:
+                msg_logger.reset_start_time()
+            if current_iter % opt['logger']['print_freq'] == 0:
+                log_vars = {'epoch': epoch, 'iter': current_iter}
+                log_vars.update({'lrs': model.get_current_learning_rate()})
+                log_vars.update({'time': iter_timer.get_current_time(), 'data_time': data_timer.get_current_time()})
+                log_vars.update({'avg_time': iter_timer.get_avg_time(), 'avg_data_time': data_timer.get_avg_time()})
+                log_vars.update(model.get_current_log())
+                msg_logger(log_vars)
+
+            if current_iter % opt['logger']['save_checkpoint_freq'] == 0:
+                logger.info('Saving models and training states.')
+                model.save(epoch, current_iter)
+
+            if opt.get('val') is not None and (current_iter % opt['val']['val_freq'] == 0):
+                for val_loader in val_loaders:
+                    model.validation(val_loader, current_iter, tb_logger)
+
+            data_timer.start()
+            iter_timer.start()
+            train_data = prefetcher.next()
+        # end of iter
+    # end of epoch
+
+    consumed_time = str(datetime.timedelta(seconds=int(time.time() - start_time)))
+    logger.info(f'End of training. Time consumed: {consumed_time}')
+    logger.info('Save the latest model.')
+    model.save(epoch=-1, current_iter=-1)  # -1 stands for the latest
+    if opt.get('val') is not None:
+        for val_loader in val_loaders:
+            model.validation(val_loader, current_iter, tb_logger)
+    if tb_logger:
+        tb_logger.close()
+
+if __name__ == '__main__':
+    main()
